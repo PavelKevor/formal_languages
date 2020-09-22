1d90b903360feebccd7ab7e3b35da854410e49df
PavelKevor <vila-kato@mail.ru>
2020-09-22 23:22:20 +0300

    Update tests.py

--- src/tests.py
+++ src/tests.py
@@ -24,7 +24,7 @@ def test_intersection_of_graphs():
     
     assert intersection.num == 28
     
-    print(garph.label_matix)
+    print(graph.label_matix)
     print(intersection.label_matrix)
 
     
