dee81d5f3451f6a68878bacc5bd95554c89cfb93
PavelKevor <vila-kato@mail.ru>
2020-11-13 23:50:22 +0300

    Update syntax_analyze_tests.py

--- src/graph_db/syntax_analyze_tests.py
+++ src/graph_db/syntax_analyze_tests.py
@@ -23,25 +23,25 @@ def test_check_syntax():
     script = "connect '/home/fl/graphs'"
     assert check_syntax_with_cyk(script)
 
-    script = "select count from graph1"
+    script = "select count from 'graph1'"
     assert check_syntax_with_cyk(script)
 
-    script = "select count from graph intersect {'a' plus conc 'b'}"
+    script = "select count from 'graph' intersect {'a' plus conc 'b'}"
     assert check_syntax_with_cyk(script)
 
-    select = "select count from graph intersect {'a' option conc 'b' conc 'c' plus}"
+    select = "select count from 'graph' intersect {'a' option conc 'b' conc 'c' plus}"
     assert check_syntax_with_cyk(script)
 
-    script = "select edges from graph intersect {('a' alt 'b') star conc 'c' plus conc 'd' option}'"
+    script = "select edges from 'graph' intersect {('a' alt 'b') star conc 'c' plus conc 'd' option}'"
     assert check_syntax_with_cyk(script)
 
-    script = "select edges from graph1 intersect graph2"
+    script = "select edges from 'graph1' intersect 'graph2'"
     assert check_syntax_with_cyk(script)
 
-    script = "count select from graph"
+    script = "count select from 'graph'"
     assert not check_syntax_with_cyk(script)
 
-    script = "select chount from graph"
+    script = "select chount from 'graph'"
     assert not check_syntax_with_cyk(script)
 
     script = "connect connect '/home/fl/graphs' "
